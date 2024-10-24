2e865256e36959a717f6fbd3ad7e6089c7f63144
Lukasz Zatorski <lukasz.zatorskii@gmail.com>
2024-10-24 21:04:40 +0200

    Add Account Controller with basic view

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AccountController extends AbstractController
+{
+    #[Route('/account', name: 'account')]
+    public function index(): Response
+    {
+        $accounts = [];
+
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+            'accounts' => $accounts,
+        ]);
+    }
+}
