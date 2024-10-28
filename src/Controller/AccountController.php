1ce938adc89a2a416118d9421d8e1e0c23cdf2aa
Lukasz Zatorski <lukasz.zatorskii@gmail.com>
2024-10-28 19:25:42 +0100

    Add  pagination and show action to Accounts

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,20 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Repository\AccountRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'account')]
-    public function index(): Response
+    #[Route('/accounts', name: 'account_index')]
+    public function index(Request $request, AccountRepository $accountRepository): Response
     {
-        $accounts = [];
+        $page = $request->query->getInt('page', 1);
+        $paginator = $accountRepository->findPaginatedAccounts($page);
 
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
-            'accounts' => $accounts,
+            'accounts' => $paginator,
+            'current_page' => $page,
+            'total_pages' => ceil(count($paginator) / AccountRepository::COMMENTS_PER_PAGE),
+        ]);
+    }
+
+    #[Route('/accounts/{id}', name: 'account_show')]
+    public function show(Account $account): Response
+    {
+        return $this->render('account/show.html.twig', [
+            'controller_name' => 'AccountController',
+            'account' => $account,
         ]);
     }
 }
--- src/Repository/AccountRepository.php
+++ src/Repository/AccountRepository.php
@@ -5,39 +5,28 @@ namespace App\Repository;
 use App\Entity\Account;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @extends ServiceEntityRepository<Account>
  */
 class AccountRepository extends ServiceEntityRepository
 {
+    public const COMMENTS_PER_PAGE = 10;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Account::class);
     }
 
-    //    /**
-    //     * @return Account[] Returns an array of Account objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findPaginatedAccounts(int $page = 1): Paginator
+    {
+        $query = $this->createQueryBuilder('a')
+            ->orderBy('a.createdAt', 'DESC')
+            ->setFirstResult(($page - 1) * self::COMMENTS_PER_PAGE)
+            ->setMaxResults(self::COMMENTS_PER_PAGE)
+            ->getQuery();
 
-    //    public function findOneBySomeField($value): ?Account
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        return new Paginator($query);
+    }
 }
